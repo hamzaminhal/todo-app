718f2f15949ba252ed6ba1746ce624db918ff082
hamzaminhal <hamzaminhal2@gmail.com>
2025-08-24 11:33:49 +0500

    edit and dlt icon changed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-import { customAlert } from "./alert.js";
 import { auth, db, getAuth } from "./config.js";
 import {
   onAuthStateChanged,
@@ -172,7 +171,7 @@ function showTasks() {
               ${task}
       </div>
             <div class="actions">
-              <button class="icon-btn edit" title="Click the text to edit" data-id="${doc.id}">
+              <button class="icon-btn" title="Click the text to edit">
                 <!-- pencil icon -->
                 <svg
                   data-id="${doc.id}"
@@ -188,10 +187,10 @@ function showTasks() {
                   <path d="M12 20h9" />
                   <path
                     d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z"
-                  ></path>
+                  >
                 </svg>
               </button>
-              <button class="icon-btn delete dlt" for="del-1" title="Delete task" data-id="${doc.id}">
+              <button class="icon-btn" for="del-1" title="Delete task">
                 <!-- trash icon -->
                 <svg                  
                   data-id="${doc.id}"
